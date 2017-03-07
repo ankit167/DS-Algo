6fe9923285d3e311b987d0b08c548846cfe84c55
Ankit Agarwal <ankit.agarwal02@snapdeal.com>
2017-03-07 15:19:19 +0530

    Adding KMP code

--- string.py
+++ string.py
@@ -26,9 +26,49 @@ def longestPalindromicSubstring(s):
 					end = j
 	return s[start:end+1]
 
+#
+# Computes prefix array for KMP
+#
+def prefixCompute(f, patt, m):
+	i,j = 1,0
+	f[0] = 0
+	while i < m:
+		if patt[i] == patt[j]:
+			f[i] = j+1
+			i += 1
+			j += 1
+		elif j > 0:
+			j = f[j-1]
+		else:
+			f[i] = 0
+			i += 1
+#
+# Searches a pattern in a string using KMP algorithm.
+# Compute prefix array -> Compare string and pattern
+# T(n)- O(m+n), S(n)- O(m)
+# n- length of string, m- length of pattern
+#
+def KMP(string, patt):
+	m, n = len(patt), len(string)
+	f = [0 for i in range(m)]
+	prefixCompute(f, patt, m)
+	i,j = 0,0
+	while i < n:
+		if string[i] == patt[j]:
+			if j == m-1:
+				return i-j
+			i += 1
+			j += 1
+		elif j > 0:
+			j = f[j-1]
+		else:
+			i += 1
+	return -1
+
 def main():
 	s = raw_input()
-	print longestPalindromicSubstring(s);
+	p = raw_input()
+	print KMP(s,p)
 
 if __name__ == "__main__":
 	main()
