98c7dbaccf32facb07d8cd1b0da13f04c36a60c6
Ankit Agarwal <ankit.agarwal02@snapdeal.com>
2017-03-08 12:33:45 +0530

    Adding code to merge intervals

--- array.py
+++ array.py
@@ -8,22 +8,22 @@ from collections import deque
 # T(n)- O(n), S(n)- O(n)
 #
 def findMinInWindow(a,k):
-	n = len(a)
-	l = deque()
+    n = len(a)
+    l = deque()
 
-	for i in range(k):
-		while len(l) > 0 and a[i] <= a[l[-1]]:
-			l.pop()
-		l.append(i)
+    for i in range(k):
+        while len(l) > 0 and a[i] <= a[l[-1]]:
+            l.pop()
+        l.append(i)
 
-	for i in range(k,n):
-		print a[l[0]]
-		while len(l) > 0 and l[0] <= i-k:
-			l.popleft()
-		while len(l) > 0 and a[i] <= a[l[-1]]:
-			l.pop()
-		l.append(i)
-	print a[l[0]]
+    for i in range(k,n):
+        print a[l[0]]
+        while len(l) > 0 and l[0] <= i-k:
+            l.popleft()
+        while len(l) > 0 and a[i] <= a[l[-1]]:
+            l.pop()
+        l.append(i)
+    print a[l[0]]
 
 #
 # Given arrival and departure times of trains. Find minimum number of platforms required
@@ -31,35 +31,35 @@ def findMinInWindow(a,k):
 # T(n)- O(nlogn)
 #
 def minPlatforms(arr, dep):
-	result, platform, n = 1, 1, len(arr)
-	arr.sort()
-	dep.sort()
-	i,j = 1,0
-	while i < n and j < n:
-		if arr[i] < dep[j]:
-			platform += 1
-			i += 1
-			if platform > result:
-				result = platform
-		else:
-			platform -= 1
-			j += 1
-	return result
+    result, platform, n = 1, 1, len(arr)
+    arr.sort()
+    dep.sort()
+    i,j = 1,0
+    while i < n and j < n:
+        if arr[i] < dep[j]:
+            platform += 1
+            i += 1
+            if platform > result:
+                result = platform
+        else:
+            platform -= 1
+            j += 1
+    return result
 
 #
 # Search a key in a row wise and column wise sorted matrix.
 # T(n)- O(m+n)
 #
 def searchInaSortedMatrix(mat, m, n, key):
-	i,j = m-1, 0
-	while i >=0 and j < n:
-		if key == mat[i][j]:
-			return True
-		if key < mat[i][j]:
-			i -= 1
-		else:
-			j += 1
-	return False
+    i,j = m-1, 0
+    while i >=0 and j < n:
+        if key == mat[i][j]:
+            return True
+        if key < mat[i][j]:
+            i -= 1
+        else:
+            j += 1
+    return False
 
 #
 # A series of 0's is followed by 1's in an array. Find the start index of 1's.
@@ -67,25 +67,25 @@ def searchInaSortedMatrix(mat, m, n, key):
 # Hint: Use Binary Search, since the array is sorted.
 #
 def findStartOne(a,start,end):
-	if start > end:
-		return None
-	mid = start + (end-start)/2
-	if a[mid] == 1:
-		if mid == start or a[mid-1] == 0:
-			return mid
-		return findStartOne(a,start,mid-1)
-	else:
-		return findStartOne(a,mid+1,end)
+    if start > end:
+        return None
+    mid = start + (end-start)/2
+    if a[mid] == 1:
+        if mid == start or a[mid-1] == 0:
+            return mid
+        return findStartOne(a,start,mid-1)
+    else:
+        return findStartOne(a,mid+1,end)
 
 #
 # Returns the maximum element in the array
 #
 def findMax(a):
-	m = a[0]
-	for i in a:
-		if i > m:
-			m = i
-	return m
+    m = a[0]
+    for i in a:
+        if i > m:
+            m = i
+    return m
 
 #
 # In an array of positive integers (>= 1),
@@ -96,26 +96,68 @@ def findMax(a):
 # Output: 2
 #
 def minMissing(a):
-	a.insert(0,0)
-	m = findMax(a)
-	d = len(str(m))
-	y = int(math.pow(10,d))
-	n = len(a)
-	for i in range(1,n):
-		x = a[i]% y
-		if x >= n:
-			continue
-		a[x] += y
-	for i in range(1,n):
-		c = a[i]//y
-		if c == 0:
-			print i
-			return
+    a.insert(0,0)
+    m = findMax(a)
+    d = len(str(m))
+    y = int(math.pow(10,d))
+    n = len(a)
+    for i in range(1,n):
+        x = a[i]% y
+        if x >= n:
+            continue
+        a[x] += y
+    for i in range(1,n):
+        c = a[i]//y
+        if c == 0:
+            print i
+            return
+
+#
+# Merge given intervals
+# Input: [(1,3),(4,10),(20,25),(21,29),(2,5)]
+# Output: (1,10), (20,29)
+# Input list is a list of tuples, where for each tuple- t,
+# t[0]- starting time, t[1]- ending time of interval
+# Approach 1: Sort list in increasing order of start time. Use stack
+#             T(n)- O(nlogn), S(n)- O(n)
+# Approach 2: Sort list in non-increasing order of start time, and
+#             maintain index. T(n)- O(nlogn) (Check gfg)
+#
+def mergeOverlappingIntervals(l):
+    n = len(l)
+    if n <= 0:
+        return
+    # Sorting list based on start time
+    l.sort(key=lambda x: x[0])
+    # Creating stack
+    s = []
+    s.append(l[0])
+    for i in range(1, n):
+        t = s[-1]
+        # No overlap. current end time < Start time of the next element
+        if t[1] < l[i][0]:
+            s.append(l[i])
+        # Overlap. current end time > next start time and < next end time
+        elif t[1] < l[i][1]:
+            # Creating a new tuple with updated values
+            nt = (t[0],l[i][1])
+            # Removing previous tuple and pushing the merged tuple
+            s.pop()
+            s.append(nt)
+
+    while len(s) > 0:
+        print s[-1],
+        s.pop()
+
 
 def main():
-	a = list(map(int,raw_input().split()))
-	minMissing(a)
+    l = []
+    n = int(raw_input())
+    for i in range(n):
+        a, b = map(int, raw_input().split())
+        l.append((a,b))
+    mergeOverlappingIntervals(l)
 
 if __name__=='__main__':
-	main()
+    main()
 
