df47298e76c9dde9cf21598505d274a31e839e03
Ankit Agarwal <ankit.agarwal02@snapdeal.com>
2017-04-02 10:48:46 +0530

    Implementing strip() in Python through regex

--- misc.py
+++ misc.py
@@ -5,9 +5,23 @@ def addWithoutOperator(x, y):
 		y = carry << 1
 	print x
 
+'''
+Implementing strip() in python through regex,
+By default, white spaces are striped from left
+and right end of the string.
+
+Input: '  Hey there '
+Output: 'Hey there'
+'''
+def strip(s,c=' '):
+	regex = '^' + re.escape(c) + '+|' + re.escape(c) + '+$'
+	s = re.sub(regex, '', s)
+	return s
+
 def main():
-    x,y = map(int, raw_input().split())
-    addWithoutOperator(x,y)
+    s = '   hey there  '
+    s = strip(s)
+    print s
 
 if __name__=='__main__':
     main()
