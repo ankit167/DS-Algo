edd2636153a5e06c841debf4148c599bdca631fb
Ankit Agarwal <ankit.agarwal02@snapdeal.com>
2017-05-08 21:23:09 +0530

    Added code to check if a given word is present in the matrix or not.

--- matrix.py
+++ matrix.py
@@ -1,4 +1,5 @@
 
+
 #
 # Count the number of unique paths from a[0][0] to a[m-1][n-1]
 # We are allowed to move either right or down from a cell in the matrix.
@@ -76,14 +77,69 @@ def display(a, row, col):
             print a[i][j],
         print
 
+
+#
+# Utility function to recursively search for a word in a matrix.
+#
+def search_word_util(a, s, visited, i, j, k, m, n):
+    visited[i][j] = True
+    if k == len(s)-1:
+        return True
+    k += 1
+    i1 = i-1
+
+    while i1 <= i+1 and i1 < m:
+        j1 = j-1
+        while j1 <= j+1 and j1 < n:
+            if i1 >= 0 and j1 >= 0 and a[i1][j1] == s[k] and not visited[i1][j1]:
+                p = search_word_util(a, s, visited, i1, j1, k, m, n)
+                if p:
+                    return p
+            j1 += 1
+        i1 += 1
+
+    visited[i][j] = False
+    return False
+
+
+'''
+Given a matrix of characters. Check if a given word is present in the matrix.
+For a given cell, We are allowed to move in any of the 8 directions.
+
+Approach: DFS
+          T(n)- O(m*n)
+
+Variation:
+In case of multiple input words, use trie approach. Create a trie with the
+input words. Start from the root, Match current character with root and
+neighbouring characters with root's children, and traverse trie according.
+If we reach a leaf node of the trie successfully, it means we current processed
+string in present in the matrix.
+(Refer gfg)
+'''
+def search_word(a, s):
+    m,n = len(a), len(a[0])
+    visited = [[False for j in range(n)] for i in range(m)]
+    k = 0
+
+    for i in range(m):
+        for j in range(n):
+            if a[i][j] == s[k] and not visited[i][j]:
+                p = search_word_util(a, s, visited, i, j, k, m, n)
+                if p:
+                    return p
+    return False
+
+
 def main():
     row,col = map(int, raw_input().split())
     a = []
     for i in range(row):
-        l = list(map(int, raw_input().split()))
+        l = raw_input().split()
+        #l = list(map(int, raw_input().split()))
         a.append(l)
-    a = transpose(a,row,col)
-    display(a,row,col)
+    s = raw_input()
+    print search_word(a, s)
 
 if __name__ == "__main__":
     main()
