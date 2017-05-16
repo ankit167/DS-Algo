a54b5b84dd8fece54cf9e392ab925d19c2ce436e
Ankit Agarwal <agrawal.coolankit@gmail.com>
2017-05-16 16:41:59 +0530

    Fixed pep8 changes

--- graph.py
+++ graph.py
@@ -1,17 +1,17 @@
 
 class Graph:
-    def __init__(self,v,e):
+    def __init__(self, v, e):
         self.v = v
         self.e = e
-        self.d = {k: [] for k in range(1,v+1)}
+        self.d = {k: [] for k in range(1, v+1)}
 
-    def insert(self,u,v):
+    def insert(self, u, v):
         l = self.d.get(u)
         l.append(v)
         self.d[u] = l
 
     # If there is directed edge from vertex a to vertex b --> a knows b
-    def knows(self,a,b):
+    def knows(self, a, b):
         l = self.d[a]
         if len(l) > 0 and b in l:
             return True
@@ -26,31 +26,31 @@ class Graph:
     # (ii) Divide and conquer (iii) Use stack. Refer gfg for these methods
     def celeb(self):
         l = self.d.keys()
-        i,j = 0,len(l)-1
+        i, j = 0, len(l)-1
 
         while i < j:
-            if self.knows(l[i],l[j]) is True:
+            if self.knows(l[i], l[j]) is True:
                 c = l[j]
                 i += 1
             else:
                 c = l[i]
                 j -= 1
-                
+
         for i in l:
             if i == c:
                 continue
-            if self.knows(c,i) is False and self.knows(i,c) is True:
+            if self.knows(c, i) is False and self.knows(i, c) is True:
                 continue
             return -1
         return c
 
 
 def main():
-    v,e = list(map(int,raw_input().split()))
-    g = Graph(v,e)
+    v, e = list(map(int, raw_input().split()))
+    g = Graph(v, e)
     for i in range(e):
-        a,b = list(map(int,raw_input().split()))
-        g.insert(a,b)
+        a, b = list(map(int, raw_input().split()))
+        g.insert(a, b)
     print g.celeb()
 
 
