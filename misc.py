6f3024ba3d0fd42134350d95bddb4ecf99f09907
Ankit Agarwal <agrawal.coolankit@gmail.com>
2017-05-16 17:05:26 +0530

    Fixed pep8 changes

--- misc.py
+++ misc.py
@@ -1,7 +1,8 @@
 import sys
 import os
 
-def addWithoutOperator(x, y):
+
+def add_without_operator(x, y):
     while y != 0:
         carry = x & y
         x = x ^ y
@@ -9,15 +10,15 @@ def addWithoutOperator(x, y):
     print x
 
 
-'''
-Implementing strip() in python through regex,
-By default, white spaces are striped from left
-and right end of the string.
-
-Input: '  Hey there '
-Output: 'Hey there'
-'''
-def strip(s,c=' '):
+#
+# Implementing strip() in python through regex,
+# By default, white spaces are striped from left
+# and right end of the string.
+#
+# Input: '  Hey there '
+# Output: 'Hey there'
+#
+def strip(s, c=' '):
     regex = '^' + re.escape(c) + '+|' + re.escape(c) + '+$'
     s = re.sub(regex, '', s)
     return s
@@ -26,7 +27,7 @@ def strip(s,c=' '):
 #
 # Find the next greater number with the same set of digits of a given number.
 #
-def nextGreater(n):
+def next_greater(n):
     l = map(int, str(n))
     i = len(l)-1
     while i >= 1 and l[i-1] >= l[i]:
@@ -34,14 +35,14 @@ def nextGreater(n):
     if i <= 0:
         print 'Not possible'
         return
-    p,index,m = i-1,i-1,sys.maxint
+    p, index, m = i-1, i-1, sys.maxint
     # We can optimise this loop by doing a Binary Search
     # (since the sublist is sorted)
-    for i in range(p+1,len(l)):
+    for i in range(p+1, len(l)):
         if l[i] > l[p] and l[i] < m:
             m = l[i]
             index = i
-    l[p],l[index] = l[index],l[p]
+    l[p], l[index] = l[index], l[p]
     l[p+1:] = sorted(l[p+1:])
     ng = int(''.join(str(x) for x in l))
     print ng
@@ -65,6 +66,5 @@ def print_dir(s_path):
 def main():
     print_dir('/Users/ankit.agarwal/Documents/repo')
 
-    
-if __name__=='__main__':
+if __name__ == '__main__':
     main()
