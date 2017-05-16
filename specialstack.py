21504e15de3756c01a6ba02ec54ea5da4d683744
Ankit Agarwal <agrawal.coolankit@gmail.com>
2017-05-16 16:46:30 +0530

    Fixed pep8 changes

--- specialstack.py
+++ specialstack.py
@@ -1,67 +1,67 @@
 #!/usr/bin/python
 
-# Design a special stack where push(), pop(), getMin(), isEmpty(), isFull() take T(n)- O(1)
+
+# Design a special stack where push(), pop(), getMin(), isEmpty(), isFull()
+# take T(n)- O(1)
 class SpecialStack:
-	realStack = []
-	auxStack = []
-	top = -1
+    realStack = []
+    auxStack = []
+    top = -1
 
-	def __init__(self,size):
-		self.size = size
+    def __init__(self, size):
+        self.size = size
 
-	def isFull(self):
-		if self.top == self.size -1:
-			print 'Stack is Full'
-			return True
-		return False
+    def isFull(self):
+        if self.top == self.size-1:
+            print 'Stack is Full'
+            return True
+        return False
 
-	def isEmpty(self):
-		if self.top == -1:
-			print 'Stack is Empty'
-			return True
-		return False
+    def isEmpty(self):
+        if self.top == -1:
+            print 'Stack is Empty'
+            return True
+        return False
 
-	def push(self,data):
-		if self.isFull():
-			return
-		self.realStack.append(data)
-		self.top += 1
-		if len(self.auxStack) == 0 or data <= self.auxStack[-1]:
-			self.auxStack.append(data)
+    def push(self, data):
+        if self.isFull():
+            return
+        self.realStack.append(data)
+        self.top += 1
+        if len(self.auxStack) == 0 or data <= self.auxStack[-1]:
+            self.auxStack.append(data)
 
-	def pop(self):
-		if self.isEmpty():
-			return
-		element = self.realStack.pop()
-		self.top -= 1
-		print 'Popped Element- %s' % element
-		if len(self.auxStack) > 0 and element == self.auxStack[-1]:
-			self.auxStack.pop()
+    def pop(self):
+        if self.isEmpty():
+            return
+        element = self.realStack.pop()
+        self.top -= 1
+        print 'Popped Element- %s' % element
+        if len(self.auxStack) > 0 and element == self.auxStack[-1]:
+            self.auxStack.pop()
 
-	# T(n) = O(1)
-	def getMin(self):
-		if len(self.auxStack) == 0:
-			return
-		print self.auxStack[-1]
+    # T(n) = O(1)
+    def getMin(self):
+        if len(self.auxStack) == 0:
+            return
+        print self.auxStack[-1]
 
 
 def main():
-	s = SpecialStack(5)
-	s.push(10)
-	s.push(20)
-	s.push(30)
-	s.push(10)
-	s.push(40)
-	s.push(50)
-	s.pop()
-	s.getMin()
-	s.push(5)
-	s.getMin()
-	s.pop()
-	s.pop()
-	s.getMin()
+    s = SpecialStack(5)
+    s.push(10)
+    s.push(20)
+    s.push(30)
+    s.push(10)
+    s.push(40)
+    s.push(50)
+    s.pop()
+    s.getMin()
+    s.push(5)
+    s.getMin()
+    s.pop()
+    s.pop()
+    s.getMin()
 
-if __name__=='__main__':
-	main()
-
-
+if __name__ == '__main__':
+    main()
