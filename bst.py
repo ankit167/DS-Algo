d6523ec848fd0a9c1846eb2b58bc18da2951ef31
Ankit Agarwal <agrawal.coolankit@gmail.com>
2017-06-05 14:43:57 +0530

    Fixed pep8 errors

--- bst.py
+++ bst.py
@@ -1,34 +1,37 @@
 from dll import DoublyLinkedList as dll
 
+
 class node:
-    def __init__(self,data):
+    def __init__(self, data):
         self.data = data
         self.left = None
         self.right = None
+
 
 class Bst:
     def __init__(self):
         self.root = None
 
-    def insert(self,root,data):
+    def insert(self, root, data):
         if root is None:
             root = node(data)
             return root
         elif data <= root.data:
-            root.left = self.insert(root.left,data)
+            root.left = self.insert(root.left, data)
         else:
-            root.right = self.insert(root.right,data)
+            root.right = self.insert(root.right, data)
         return root
+
     #
     # T(n)- O(h). (Also refer to finding lca using parent pointers in gfg)
     #
-    def lca(self,root,n1,n2):
+    def lca(self, root, n1, n2):
         if root is None:
             return None
         if root.data > n1 and root.data > n2:
-            return self.lca(root.left,n1,n2)
+            return self.lca(root.left, n1, n2)
         if root.data < n1 and root.data < n2:
-            return self.lca(root.right,n1,n2)
+            return self.lca(root.right, n1, n2)
         return root
 
     def inorderAppend(self, a, root):
@@ -56,7 +59,6 @@ class Bst:
         self.convertToDLL(root.right)
         return start[0]
 
-
     #
     # Check for a pair of numbers in the BST, the sum of which is 'k'
     # Approaches-
@@ -73,8 +75,8 @@ class Bst:
     def sumK(self, root, k):
         if root is None:
             return
-        stack1,stack2 = [], []
-        flag1,flag2 = True, True
+        stack1, stack2 = [], []
+        flag1, flag2 = True, True
         curr1, curr2 = root, root
         while(True):
             while(flag1):
@@ -111,9 +113,8 @@ class Bst:
                 flag2 = True
 
 
-
 def main():
-    a = list(map(int,raw_input().split()))
+    a = list(map(int, raw_input().split()))
     t = Bst()
     for i in a:
         t.root = t.insert(t.root,i)
