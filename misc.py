6ccc50547529243049dbc37c2bd0aae2d8329a60
Ankit Agarwal <agrawal.coolankit@gmail.com>
2017-06-09 09:49:16 +0530

    Code to play 'n' songs randomly, exactly once.

--- misc.py
+++ misc.py
@@ -2,6 +2,7 @@ import sys
 import os
 import re
 import math
+from random import randint
 
 
 #
@@ -106,9 +107,35 @@ def print_primes(n):
             print i,
 
 
+#
+# Given a music player with 'n' songs. Play a song randomly, making
+# sure that each song gets played exactly once. The program should
+# terminate once all songs have been played.
+#
+# Approach: Find a random song in the list. Play the song. Swap the
+#           song with the last song in the list. Reduce length of the list
+#           by 1. Iterate with this logic until the length of the list gets
+#           reduced to 0.
+#
+# T(n)- O(n)
+#
+# Note: The hashmap approach (to hash a visited song and check whether
+#       it has been played before) might not work, since the random function
+#       might return the same song again and again, leading to infinite loop.
+#
+def music_player(a):
+    n = len(a)
+    s = set()
+    while n > 0:
+        num = randint(0, n-1)
+        print "Playing song %s" % a[num]
+        a[num], a[n-1] = a[n-1], a[num]
+        n -= 1
+
+
 def main():
-    n = int(raw_input())
-    count_ones(n)
+    l = raw_input().split()
+    music_player(l)
 
 if __name__ == '__main__':
     main()
