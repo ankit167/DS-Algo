e15654e3333a7952ee8786457f2965953f0aa041
Ankit Agarwal <agrawal.coolankit@gmail.com>
2017-06-23 13:24:50 +0530

    Implement queue using 2 stacks

--- stack.py
+++ stack.py
@@ -80,6 +80,54 @@ class Stack:
         self.sortStack()
         self.sortedInsert(data)
 
+    #
+    # Implement Queue using two stacks.
+    # Making 'dequeue' operation expensive
+    #
+    def queue_using_two_stacks1(self):
+        st1, st2 = [], []
+
+        # T(n)- O(1)
+        def enqueue(data):
+            print 'Inserting %s' % data
+            st1.append(data)
+
+        # T(n)- O(n)
+        def dequeue():
+            if len(st2) > 0:
+                return st2.pop()
+
+            while len(st1) > 0:
+                t = st1.pop()
+                st2.append(t)
+            return st2.pop()
+
+    #
+    # Implement Queue using two stacks.
+    # Making 'enqueue' operation expensive
+    #
+    def queue_using_two_stacks2(self):
+        st1, st2 = [], []
+
+        # T(n)- O(n)
+        def enqueue(data):
+            while len(st2) > 0:
+                t = st2.pop()
+                st1.append(t)
+
+            print 'Inserting %s' % data
+            st2.append(data)
+
+            while len(st1) > 0:
+                t = st1.pop()
+                st2.append(t)
+
+        # T(n)- O(1)
+        def dequeue():
+            while len(st2) > 0:
+                return st2.pop()
+            return -1
+
 
 def main():
     s = Stack()
