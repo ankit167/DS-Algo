819e63315f4ebe1c60f71fc5f73f850d6f6c6533
Ankit Agarwal <agrawal.coolankit@gmail.com>
2019-06-22 18:19:41 +0530

    Remove redundant code

--- sample.py
+++ sample.py
@@ -111,25 +111,6 @@ def compare(a, b):
     return -1
 
 
-def printspir(a, m, n):
-    k, l = 0, 0
-    while k < m and l < n:
-        for i in range(l, n):
-            print a[k][i],
-        k += 1
-        for i in range(k, m):
-            print a[i][n-1],
-        n -= 1
-        if k < m:
-            for i in range(n-1, l-1, -1):
-                print a[m-1][i],
-            m -= 1
-        if l < n:
-            for i in range(m-1, k-1, -1):
-                print a[i][l],
-            l += 1
-
-
 def printMaxNumber(s):
     s.sort(compare)
     print s
